4cb146b0cfb1b239002824e8e2933b28448332e8
Alexia Gregorio Dias <alexiagregorio6@hotmail.com>
2023-02-14 15:45:59 -0300

    13

---
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/video13/script.js
@@ -0,0 +1,12 @@
+let age = 30;
+age = 31;
+
+const birthYear = 1991; //uma constante não pode ser alterada depois
+// birthYear = 1990; // vai dar erro aqui pq não da pra alterar
+console.log(birthYear);
+
+var job = 'programmer'; //avoid using "var"
+job = 'teacher'
+
+lastName = 'Gregorio' //não escreva variaveis assim sem declarar, é uma péssima ideia, então sempre declare antes usando "const", "let" ou até "var"
+console.log(lastName)
