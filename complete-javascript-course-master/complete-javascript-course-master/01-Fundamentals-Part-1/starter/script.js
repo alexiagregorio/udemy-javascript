b4161857c50f667f97a91b0efe881633bd7f51eb
Alexia Gregorio Dias <alexiagregorio6@hotmail.com>
2023-02-14 15:36:36 -0300

    att

---
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,39 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log('23');
+
+let firstName = 'Jonas';
+let first = 'jonas'
+let firstNamePerson = 'bob'
+let first_name_person = 'alex'
+
+console.log(firstName);
+
+let myFirstJob = 'Teacher'
+let myCurrentJob = 'Programmer'
+
+console.log(myFirstJob)
+
+/////////////////////////////////////////////////
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let year; //it's empty that's why the typeof is undefined
+console.log(year);
+console.log(typeof year);
+
+year = 1991; //now it has a value so the typeof is number
+console.log(typeof year);
+
+console.log(typeof null); //null is an object
