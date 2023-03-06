46689c2837e2cdf99d88739c80abc46e485bcf81
Alexia Gregorio Dias <alexiagregorio6@hotmail.com>
2023-03-06 13:47:32 -0300

    fundamentals 2 - video 32

---
+++ complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/video 32 - strict mode/script.js
@@ -0,0 +1,10 @@
+'use strict'; //usar logo no começo
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriversLicense = true;
+if(hasDriversLicense) console.log('I can drive :D')
+
+//const insterface = 'Audio'
+//const provate = 534;
